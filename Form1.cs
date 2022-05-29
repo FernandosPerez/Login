ba3e0b432eba655fcc10e19677a6642e0e7b8ef8
ferna <ferna@LA_MAMALONCITA>
2022-05-28 23:50:54 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+
+    
+    public partial class Form1 : Form
+    {
+
+        public static int intentos;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (intentos > 3)
+            { 
+
+                Form2 form = new Form2();
+                form.Show();
+
+            }
+
+            if (textBox2.Text != "gato")
+            {
+                intentos = intentos + 1;
+
+                if (intentos == 1)
+                {
+                    MessageBox.Show("has tenido un error");
+                }else if (intentos > 1 && intentos <5)
+                {
+                    MessageBox.Show("has tenido "+intentos+" errores");
+                }    
+                
+
+            }
+
+            if (textBox2.Text == "gato")
+            {
+                
+                Form3 form = new Form3();
+                form.Enabled = true;
+                form.Show();
+                
+                
+            }
+            
+
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form2 : Form
+    {
+        int falla = 4;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Enabled ==true )
+            {
+                button3.Enabled = true;
+
+                if (textBox1.Text=="5576945365")
+                {
+                    this.Close();
+                    Form1.intentos = 0;
+                }else if (textBox1.Text!= "5576945365")
+                {
+                    falla = falla -1;
+                    MessageBox.Show("El numero no corresponde tienes "+falla+ " intentos");
+                    if (falla == 0)
+                    {
+                        button3.Enabled = false;
+                        textBox1.Enabled = false;
+                        MessageBox.Show("Se te dijo y se te advirtio !!!");
+                    }
+                }
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Enabled == true)
+            {
+                button3.Enabled = true;
+            }
+        }
+    }
+}
